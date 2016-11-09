d6c54d4f73b2399bbe277468d936ab013d7d13eb
Momus <air_jordan981182@yahoo.com.tw>
2016-11-09 12:48:37 +0800

    Data Type

---
+++ 04Data Type.py
@@ -0,0 +1,38 @@
+#04 Data Type
+
+
+#set x as int
+x = 123
+print(type(x))
+
+#set x as str
+x = '123'
+print(type(x))
+
+#set x as float
+x = 123.12
+print(type(x))
+
+#set x as complex
+x = 123+2j
+print(type(x))
+
+#set x as bool
+x = True
+print(type(x))
+
+#set x as tuple
+x = (123,456)
+print(type(x))
+
+#set x as list
+x = [123]
+print(type(x))
+
+#set x as set
+x = {123}
+print(type(x))
+
+#set x as dictv
+x = {1:123}
+print(type(x))
