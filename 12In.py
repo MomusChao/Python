b7be7e74033bdec75ccc6710efb7b3fa6e1a9ec2
Momus <air_jordan981182@yahoo.com.tw>
2016-11-12 16:53:41 +0800

    In

---
+++ 12In.py
@@ -0,0 +1,13 @@
+#12 in
+
+tuple = (1,2,3,'4',[1,2])
+list = [5,6,7,'8']
+dictionary = {9:'b',0:'d'}
+set = {'x','y','z'}
+string = 'abce'
+
+print(1 in tuple, 4in tuple, 9 not in tuple)
+print(5 in list, 8 in list, 'a' not in list)
+#check "key"
+print('b' in dictionary, 9 in dictionary)
+print('c'in string)
