a351eb795739ab8149f633a49063293a9b2fd48c
Momus <air_jordan981182@yahoo.com.tw>
2016-11-12 16:41:18 +0800

    Comparison Operators

---
+++ 11Comparison Operators.py
@@ -0,0 +1,28 @@
+#11 Comparison Operators
+
+print(2==2)
+print(2!=2)
+print(6>4)
+print(6<4)
+print(6>=2)
+print(6<=2)
+print('',end='\n')
+
+
+list1 = [1,'Hi']
+list2 = [1,'Hi']
+
+print(list1 == list2)
+#target object is the same?
+print(list1 is list2)
+
+list1 = list2
+
+print(list1 == list2)
+print(list1 is list2)
+
+a = 'hii'
+b = 'hii'
+
+print(a == b)
+print(a is b)
