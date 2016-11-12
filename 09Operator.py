a64b5fd7eb1998941e0eab9e5f6161f554f76b46
Momus <air_jordan981182@yahoo.com.tw>
2016-11-12 15:40:27 +0800

    Operator

---
+++ 09Operator.py
@@ -0,0 +1,15 @@
+#09 Operator
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+
+#exponential operator
+print(2**3)
+
+#division: take integer only
+print(5//2)
+
+#division: take remainder only
+print(100%3)
