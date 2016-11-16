60eea90d55b648cb11e17621fd1013082e2dd504
Momus <air_jordan981182@yahoo.com.tw>
2016-11-17 01:10:42 +0800

    str.count()

---
+++ 22str.count().py
@@ -0,0 +1,13 @@
+#22 str.count()
+
+#str.count(sub[,start[,end]])
+string = 'I have an apple, I have a pen.'
+
+#count 'a'
+print(string.count('a'))
+
+#count 'a' from index=6
+print(string.count('a',6))
+
+#count 'a' from index=0 to index=36 (include 36)
+print(string.count('a',0,36))
