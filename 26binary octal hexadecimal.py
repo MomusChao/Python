bb7211e8c1d06a7e2ce5129339e2f051c4b0cfd2
Momus <air_jordan981182@yahoo.com.tw>
2016-11-19 17:21:25 +0800

    binary octal hexadecimal

---
+++ 26binary octal hexadecimal.py
@@ -0,0 +1,15 @@
+#26 binary octal hexadecimal
+
+#change number into binary number
+print(bin(2354677))
+#change number into octal
+print(oct(2354677))
+#change number into hexadecimal
+print(hex(2354677))
+
+#ob: number in binary number
+print(0b1000111110110111110101)
+#0o: number in Octal
+print(0o10766765)
+#0x: number in hexadecimal
+print(0x23edf5)
