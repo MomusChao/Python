106e18212f51192b40b272a787f4e878c4eb96ad
Momus <air_jordan981182@yahoo.com.tw>
2016-11-22 14:27:50 +0800

    if else

---
+++ 35if else.py
@@ -0,0 +1,14 @@
+#35 if else
+
+#35-1
+age = input(' Your age:')
+if(int(age)>=18):
+    print("You're adult")
+else:
+    print("Your're not adult")
+
+#35-2
+if True:
+    print("Always print")
+
+
