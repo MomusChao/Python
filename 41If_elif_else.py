c3f1e47be37d4063b3e4733e319c9d3fdab0ca90
Momus <air_jordan981182@yahoo.com.tw>
2016-12-01 12:25:50 +0800

    If_elif_else

---
+++ 41If_elif_else.py
@@ -0,0 +1,9 @@
+#41 if...elif...else
+
+age = input('Input your age: ')
+if int(age)<=20:
+    print('you are teenager')
+elif int(age)>=65:
+    print('you are older')
+else:
+    print('you are adult')
