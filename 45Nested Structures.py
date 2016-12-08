410697c6316c92780d6a25e6f8faab01b12a87c9
Momus <air_jordan981182@yahoo.com.tw>
2016-12-08 09:15:10 +0800

    Nested Structures

--- 45Nested Structures.py
+++ 45Nested Structures.py
@@ -2,7 +2,8 @@
 
 i=j=1
 for i in range(2,100,1):
-    #i/j show Divisor, then break
+    # number can't less than 2
+    #i/j divisor have 2, then break
     for j in range(2,int(i/j)+1):
         if(not i%j):
             break
