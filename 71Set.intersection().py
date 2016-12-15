d8cc559da95bc495df561e8191a4a06beb8f7162
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 11:11:47 +0800

    Set.intersection()

---
+++ 71Set.intersection().py
@@ -0,0 +1,11 @@
+#71 Set.intersection()
+
+#set.intersection(set2)
+cities01 = {'Taipei', 'Tokyo','New York'}
+print(cities01,'\n',sep="")
+cities02 = {'Hanoi', 'Tokyo','Berlin'}
+print(cities02,'\n',sep="")
+
+
+cities03 = cities01.intersection(cities02)
+print(cities03,'\n',sep="")
