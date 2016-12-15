af6a6bbc72b55e9431b7ebb153e94e360e3c30ba
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 11:13:09 +0800

    Set.update()

---
+++ 74Set.update().py
@@ -0,0 +1,12 @@
+#74 Set.update()
+
+#update for union
+#set.update(set2)
+cities01 = {'Taipei', 'Tokyo','New York'}
+print(cities01,'\n',sep="")
+cities02 = {'Hanoi', 'Tokyo','Berlin'}
+print(cities02,'\n',sep="")
+
+
+cities01.update(cities02)
+print(cities01,'\n',sep="")
