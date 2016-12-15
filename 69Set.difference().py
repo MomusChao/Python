da72b6ebdb7ec3ab6c29a92fbb334503e7cb288f
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 11:11:07 +0800

    Set.difference()

---
+++ 69Set.difference().py
@@ -0,0 +1,11 @@
+#69 Set.difference()
+
+#set.difference(set2)
+cities01 = {'Taipei', 'Tokyo','New York'}
+print(cities01,'\n',sep="")
+cities02 = {'Hanoi', 'Tokyo','Berlin'}
+print(cities02,'\n',sep="")
+
+
+cities03 = cities01.difference(cities02)
+print(cities03,'\n',sep="")
