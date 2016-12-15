48c84c992e300ccda301c20992d36a7e7c294ed2
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 10:38:24 +0800

    Tuple

---
+++ 49Tuple.py
@@ -0,0 +1,9 @@
+#49 Tuple
+
+#ordered data (like List& Str),and it can't be changed
+
+a = ('a','b','mary','Bob','mary')
+
+#find "mary" from index 1 to 5
+print(a.index('Bob',1,5))
+print(a.count('mary'))
