004916080047df0deb828c6a373901856122982a
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 09:16:11 +0800

    Nested Structures

--- 45Nested Structures.py
+++ 45Nested Structures.py
@@ -1,10 +1,16 @@
 #45 Nested Structures
 
+#print prime number from 2 to 100
+
 i=j=1
+
+#number from 2 to 100
 for i in range(2,100,1):
+    
     # number can't less than 2
-    #i/j divisor have 2, then break
+    
     for j in range(2,int(i/j)+1):
+        #divisible: break
         if(not i%j):
             break
     if j>i**0.5:
