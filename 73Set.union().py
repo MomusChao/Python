d44d2f5584a4a42c781f5e11de16d48758de151d
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 11:12:47 +0800

    Set.union()

---
+++ 73Set.union().py
@@ -0,0 +1,11 @@
+#73 Set.union()
+
+#set.union(set2)
+cities01 = {'Taipei', 'Tokyo','New York'}
+print(cities01,'\n',sep="")
+cities02 = {'Hanoi', 'Tokyo','Berlin'}
+print(cities02,'\n',sep="")
+
+
+cities03 = cities01.union(cities02)
+print(cities03,'\n',sep="")
