b8c83e2f064de515822a1a4739c36dc8b19c7b19
Momus <air_jordan981182@yahoo.com.tw>
2016-12-15 14:10:52 +0800

    Function

---
+++ 83Function.py
@@ -0,0 +1,12 @@
+#83 Function
+
+#def function name(aparameters):
+#    function_suite
+#    return[expression]
+
+def FunctionTest(str):
+    print(str)
+    return
+
+FunctionTest("call Function")
+FunctionTest("test, again")
